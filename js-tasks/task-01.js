9d7ed8b3253c5d68f8249bb69e00e1e4b70d84d2
Oleksii Pavliuk <141736751+OleksiiPavliuk2002@users.noreply.github.com>
2024-11-17 17:17:28 +0000

    Added task-01

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -53,3 +53,13 @@ function lovefunc(flower1, flower2) {
 
   return false;
 }
+
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
+  } else if (flower1 % 2 !== 0 && flower2 % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
