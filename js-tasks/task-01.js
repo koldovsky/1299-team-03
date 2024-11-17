0092ee4cb82a89e59e6ac360d538906f740b8c0b
s-selutina <80218127+s-selutina@users.noreply.github.com>
2024-11-17 19:36:33 +0000

    Added js code

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -1,3 +1,4 @@
+// Messi Goals
 // https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 
 function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
@@ -6,20 +7,17 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 // Pavlo Onyshko code
 
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
-const Test = require('@codewars/test-compat');
+//Seliutina Svitlana
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  const sumGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return sumGoals;
+}
 
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(goals(0,0,0), 0)
-    Test.assertEquals(goals(43, 10, 5), 58)
-  });
-});
-
-
+// Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
 function makeNegative(num) {
@@ -36,14 +34,16 @@ function makeNegative(num) {
   return -Math.abs(num);
 }
 
-const Test = require('@codewars/test-compat');
+//Seliutina Svitlana
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
 
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(makeNegative(42), -42);
-  });
-});
-
+//Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
 function move(position, roll) {
@@ -56,16 +56,13 @@ function move(position, roll) {
   return position + roll * 2;
 }
 
-const Test = require('@codewars/test-compat');
+//Seliutina Svitlana
 
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(move(0, 4), 8);
-    Test.assertEquals(move(3, 6), 15);
-    Test.assertEquals(move(2, 5), 12);
-  });
-});
+function move(position, roll) {
+  return position + roll * 2;
+}
 
+//Personalized Message
 // https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 
 function greet(name, owner) {
@@ -86,15 +83,12 @@ function greet(name, owner) {
   }
 }
 
-const Test = require('@codewars/test-compat');
+//Seliutina Svitlana
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
 
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(greet('Alex', 'Alex'), 'Hello boss');
-    Test.assertEquals(greet('Denis', 'Alex'), 'Hello guest');
-  });
-});
-
+//Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
 function litres(time) {
@@ -107,20 +101,12 @@ function litres(time) {
   return Math.floor(time * 0.5);
 }
 
-const Test = require('@codewars/test-compat');
+//Seliuna Svitlana
+function litres(time) {
+  return Math.trunc(time * 0.5);
+}
 
-describe("Tests", () => {
-  it("Fixed tests", () => {
-    Test.assertEquals(litres(2), 1);
-    Test.assertEquals(litres(1.4), 0);
-    Test.assertEquals(litres(12.3), 6);
-    Test.assertEquals(litres(0.82), 0);
-    Test.assertEquals(litres(11.8), 5);
-    Test.assertEquals(litres(1787), 893);
-    Test.assertEquals(litres(0), 0);
-  });
-});
-
+//Opposites Attract
 // https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 
 function isEven(number) {
@@ -151,17 +137,18 @@ function lovefunc(flower1, flower2) {
 
 // Pavlo Onyshko code
 
-function lovefunc(flower1, flower2){
+function lovefunc(flower1, flower2) {
   return (flower1 + flower2) % 2 === 1;
 }
 
-const Test = require('@codewars/test-compat');
-
-describe("Tests", () => {
-  it("test", () => {
-    Test.assertEquals(lovefunc(1, 4), true);
-    Test.assertEquals(lovefunc(2, 2), false);
-    Test.assertEquals(lovefunc(0, 1), true);
-    Test.assertEquals(lovefunc(0, 0), false);
-  });
-});
+//Seliuna Svitlana
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 === 1 && flower2 % 2 === 0) ||
+    (flower1 % 2 === 0 && flower2 % 2 === 1)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
