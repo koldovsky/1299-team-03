46cd21021f006592bd86bd899cbfbb2cdc162697
Oleksii Pavliuk <141736751+OleksiiPavliuk2002@users.noreply.github.com>
2024-11-18 07:24:47 +0000

    Updated task 1 and added new decisions

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -17,6 +17,11 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return sumGoals;
 }
 
+//Oleksii Pavliuk
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
 // Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
@@ -43,6 +48,15 @@ function makeNegative(num) {
   }
 }
 
+//Oleksii Pavliuk
+function makeNegative(num) {
+  if (num > 0) {
+    return(num * -1);
+  } else {
+    return(num);
+  }
+}
+
 //Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
@@ -59,6 +73,11 @@ function move(position, roll) {
 //Seliutina Svitlana
 
 function move(position, roll) {
+  return position + roll * 2;
+}
+
+//Oleksii Pavliuk
+function move (position, roll) {
   return position + roll * 2;
 }
 
@@ -88,6 +107,15 @@ function greet(name, owner) {
   return name === owner ? "Hello boss" : "Hello guest";
 }
 
+//Oleksii Pavliuk
+function greet (name, owner) {
+  if (name == owner) {
+    return 'Hello boss';
+  } else {
+    return 'Hello guest';
+  }
+}
+
 //Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
@@ -104,6 +132,11 @@ function litres(time) {
 //Seliuna Svitlana
 function litres(time) {
   return Math.trunc(time * 0.5);
+}
+
+//OLeksii Pavliuk
+function litres(time) {
+  return Math.floor(time * 0.5);
 }
 
 //Opposites Attract
@@ -125,6 +158,7 @@ function lovefunc(flower1, flower2) {
   return false;
 }
 
+//Oleksii Pavliuk
 function lovefunc(flower1, flower2) {
   if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
     return true;
