f34fafb116d08d039e82aeac5ff274613ad296f8
BogdanPelenskiy <80599403+BogdanPelenskiy@users.noreply.github.com>
2024-11-18 16:12:42 +0200

    Writed my solution exersizes

--- js-tasks/task-01.js
+++ js-tasks/task-01.js
@@ -22,6 +22,12 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
+//Bohdan Pelenskyi
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let totalGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return totalGoals;
+}
+
 // Make negative
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
@@ -57,6 +63,15 @@ function makeNegative(num) {
   }
 }
 
+//Bohdan Pelenskyi
+function makeNegative(number) {
+  if (number > 0) {
+    return -number;
+  } else {
+    return number;
+  }
+}
+
 //Game Move
 // https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 
@@ -79,6 +94,11 @@ function move(position, roll) {
 //Oleksii Pavliuk
 function move (position, roll) {
   return position + roll * 2;
+}
+
+//Bohdan Pelenskyi
+function move (position, roll) {
+  return position + roll * 2
 }
 
 //Personalized Message
@@ -116,6 +136,14 @@ function greet (name, owner) {
   }
 }
 
+//Bohdan Pelenskyi
+function greet (name, owner) {
+  if (name === owner) {
+    return 'Hello boss';
+  }
+  return 'Hello guest';
+}
+
 //Keep Hydrated
 // https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 
@@ -135,6 +163,11 @@ function litres(time) {
 }
 
 //OLeksii Pavliuk
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+//Bohdan Pelenskyi
 function litres(time) {
   return Math.floor(time * 0.5);
 }
@@ -180,6 +213,19 @@ function lovefunc(flower1, flower2) {
   if (
     (flower1 % 2 === 1 && flower2 % 2 === 0) ||
     (flower1 % 2 === 0 && flower2 % 2 === 1)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+//Bohdan Pelenskyi
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 == 1 && flower2 % 2 == 0) ||
+    (flower1 % 2 == 0 && flower2 % 2 == 1)
   ) {
     return true;
   } else {
