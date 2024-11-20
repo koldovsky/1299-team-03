d97c00f92dc0a89cd505e991af1842ed44d23f2e
Stas <90963138+StasIde@users.noreply.github.com>
2024-11-20 09:37:51 +0000

    added answers for task-02

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -14,6 +14,11 @@ function DNAtoRNA(dna) {
   return dna.replaceAll("T", "U");
 }
 
+// Sydorov Stas
+function DNAtoRNA(dna) {
+  return dna.split("").map(el => el === "T" ? "U" : el).join("")
+}
+
 //Find Maximum and Minimum Values of a List
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
@@ -29,3 +34,18 @@ var max = function (list) {
   listCopy.sort((a, b) => b - a);
   return listCopy[0];
 };
+
+// Sydorov Stas
+const min = function (list) {
+  return [...list].sort((a, b) => a - b)[0];
+};
+
+const max = function (list) {
+  return [...list].sort((a, b) => b - a)[0];
+};
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+// Sydorov Stas
+const min = (arr, toReturn) => toReturn === "value" ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+
