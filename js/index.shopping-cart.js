fc25fa176a9ceb4e89d28c71768b0c85eb5edda0
Stepan Seni <stepanseniak@gmail.com>
2024-11-25 22:28:26 +0200

    feat: Add shopping cart toggle logic and adjust CSS for display
        - Implemented click event listener for the basket element to toggle the shopping cart visibility.
        - Update shopping-cart-container CSS.

---
+++ js/index.shopping-cart.js
@@ -0,0 +1,5 @@
+document.querySelector(".basket").addEventListener("click", () => {
+    const cart = document.querySelector(".shopping-cart-container");
+    cart.style.display = "flex"; 
+  });
+  
