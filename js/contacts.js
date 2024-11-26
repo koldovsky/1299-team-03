de65d9362fef10e768d40e6a970223fc1ae11cdd
Stepan Seni <stepanseniak@gmail.com>
2024-11-26 19:03:21 +0200

    feat: Add logic to close the shopping cart when clicking outside its boundaries

--- js/contacts.js
+++ js/contacts.js
@@ -1,5 +1,6 @@
 function init() {
     import("./header.js");
+    import("./global.shopping-cart.js");
   }
   
   const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
--- js/global.shopping-cart.js
+++ js/global.shopping-cart.js
@@ -1,7 +1,9 @@
 const cartContainer = document.querySelector(".shopping-cart-container");
 const cartCloseButton = document.querySelector(".shopping-cart__close-button");
+const cartOverlay = document.querySelector(".shopping-cart__overlay");
 
 cartCloseButton.addEventListener("click", hideCard);
+cartOverlay.addEventListener("click", hideCard);
 
 function hideCard() {
   cartContainer.style.display = "none";
