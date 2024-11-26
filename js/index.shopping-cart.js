a94597bc58ba399a73a65086a96793c03e95d674
Stas <90963138+StasIde@users.noreply.github.com>
2024-11-26 08:39:28 +0000

    update shopping-card js logic

--- js/index.shopping-cart.js
+++ js/index.shopping-cart.js
@@ -1,22 +1,8 @@
-const basket = document.querySelector(".basket");
 const cartContainer = document.querySelector(".shopping-cart-container");
-const headerOrderButton = document.querySelector(".header__order-button");
 const cartCloseButton = document.querySelector(".shopping-cart__close-button");
 
-basket.addEventListener("click", () => {
-  cart.style.display = "flex";
-});
+cartCloseButton.addEventListener("click", hideCard);
 
-cartContainer.addEventListener("click", (event) => {
-  if (event.target === event.currentTarget) {
-    cart.style.display = "none";
-  }
-});
-
-headerOrderButton.addEventListener("click", () => {
-  cart.style.display = "flex";
-});
-
-cartCloseButton.addEventListener("click", () => {
-  cart.style.display = "none";
-});
+function hideCard() {
+  cartContainer.style.display = "none";
+}
