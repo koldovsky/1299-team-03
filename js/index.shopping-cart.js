3ce68925ecd2552c45a1a274e53885c9a9c62e43
Stepan Seni <stepanseniak@gmail.com>
2024-11-26 09:16:05 +0200

    feat: Add logic for  closing shopping cart window
        - Added a click event listener to the shopping cart container to close the cart when clicking outside its content.
        - Added a click event listener to the shopping cart close button to hide the cart.

--- js/index.shopping-cart.js
+++ js/index.shopping-cart.js
@@ -1,5 +1,18 @@
-document.querySelector(".basket").addEventListener("click", () => {
-    const cart = document.querySelector(".shopping-cart-container");
-    cart.style.display = "flex"; 
-  });
-  
+const basket = document.querySelector(".basket");
+const cart = document.querySelector(".shopping-cart-container");
+const cartContainer = document.querySelector(".shopping-cart-container");
+const cartCloseButton = document.querySelector(".shopping-cart__close-button");
+
+basket.addEventListener("click", () => {
+  cart.style.display = "flex";
+});
+
+cartContainer.addEventListener("click", (event) => {
+  if (event.target === event.currentTarget) {
+    cart.style.display = "none";
+  }
+});
+
+cartCloseButton.addEventListener("click", () => {
+  cart.style.display = "none";
+});
