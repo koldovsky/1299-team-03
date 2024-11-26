e0058f236b5fa7376d728b8d757999cd61cfe1b6
Stepan Seni <stepanseniak@gmail.com>
2024-11-26 21:37:00 +0200

    fix: Add total amount calculation logic in shopping cart

--- js/global.shopping-cart.js
+++ js/global.shopping-cart.js
@@ -1,6 +1,10 @@
 const cartContainer = document.querySelector(".shopping-cart-container");
 const cartCloseButton = document.querySelector(".shopping-cart__close-button");
 const cartOverlay = document.querySelector(".shopping-cart__overlay");
+const quantityInput = document.querySelector("#product-quantity");
+const totalAmount = document.querySelector(".shopping-cart__total-amount");
+const unitPrice = 299;
+
 
 cartCloseButton.addEventListener("click", hideCard);
 cartOverlay.addEventListener("click", hideCard);
@@ -8,3 +12,9 @@ cartOverlay.addEventListener("click", hideCard);
 function hideCard() {
   cartContainer.style.display = "none";
 }
+
+quantityInput.addEventListener("input", () => {
+  const quantity = parseInt(quantityInput.value, 10);
+  const total = unitPrice * quantity;
+  totalAmount.textContent = `$${total.toFixed(2)}`;
+});
