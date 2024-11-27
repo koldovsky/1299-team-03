5f1a5048a1b073f133a761a2880b81d2cf654a4b
Stas <90963138+StasIde@users.noreply.github.com>
2024-11-27 15:53:19 +0000

    update order-button listeners

--- js/global.shopping-cart.js
+++ js/global.shopping-cart.js
@@ -6,9 +6,10 @@ const cartOverlay = document.querySelector(".shopping-cart__overlay");
 const quantityInput = document.querySelector("#product-quantity");
 const productPrice = document.querySelector(".shopping-cart__product-price");
 const totalAmount = document.querySelector(".shopping-cart__total-amount");
-const headerOrderButton = document.querySelector(".header__order-button");
+const orderButtons = document.querySelectorAll(".order-button");
 const cart = document.querySelector(".shopping-cart-container");
 const basket = document.querySelector(".basket-container");
+console.log(orderButtons);
 
 const unitPrice = 299;
 let productCount = 0;
@@ -16,7 +17,9 @@ let productCount = 0;
 cartCloseButton.addEventListener("click", hideCard);
 cartOverlay.addEventListener("click", hideCard);
 quantityInput.addEventListener("input", setProducts);
-headerOrderButton.addEventListener("click", showCardAndAddProduct);
+orderButtons.forEach((button) => {
+  button.addEventListener("click", showCardAndAddProduct);
+});
 basket.addEventListener("click", showCard);
 
 function setProducts({ target }) {
