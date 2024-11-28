c4a85beeafa10052e60d6804404d61f0bdb1bf4b
Stas <90963138+StasIde@users.noreply.github.com>
2024-11-28 20:46:09 +0000

    create hideMenu function and added to orderButton and to basket click

--- js/header.js
+++ js/header.js
@@ -2,15 +2,19 @@ const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const btnSub = document.querySelector(".header__burger");
 const navLinks = document.querySelector(".menu");
-
+const orderButton = document.querySelector(".header__order-button");
+const basket = document.querySelector(".basket-container");
 
 btnSub.addEventListener("click", () => {
   body.classList.toggle("overflow-hidden");
   header.classList.toggle("nav-visible");
 });
 
-navLinks.addEventListener("click", () => {
+navLinks.addEventListener("click", hideMenu);
+orderButton.addEventListener("click", hideMenu);
+basket.addEventListener("click", hideMenu);
+
+function hideMenu() {
   body.classList.remove("overflow-hidden");
   header.classList.remove("nav-visible");
-});
-
+}
