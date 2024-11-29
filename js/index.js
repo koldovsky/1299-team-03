4b3526da9f65d26615e5792239150514c8dcd391
Pavlo-web <pavloonisko36@gmail.com>
2024-11-29 21:12:57 +0000

    Resolved the conflict

--- js/index.js
+++ js/index.js
@@ -4,12 +4,9 @@ function init() {
   import("./index.special-order.js");
   import("./index.partner.js");
   import("./index.products-box-carousel-cards.js");
-<<<<<<< HEAD
   import("./global.shopping-cart.js");
   import("./utils.js");
-=======
   import("./index.ingredients.js");
->>>>>>> f30a902 (Fixed code ingredients.css)
 }
 
 const totalPartials = document.querySelectorAll(
