c996063051e34e1249fd969f9cd4039e93e96f41
s-selutina <80218127+s-selutina@users.noreply.github.com>
2024-12-03 05:59:28 +0000

    Added js file for contacts.terms-conditions

--- js/contacts.term-conditions.js
+++ js/contacts.term-conditions.js
@@ -1,17 +1,16 @@
-const btn = document.querySelector('.accordion__icon');
-const termText = document.querySelector('.contacts__term-text');
+const btn = document.querySelector(".accordion__icon");
+const termText = document.querySelector(".contacts__term-text");
 
-const buttons = document.querySelectorAll('.accordion__icon');
-const termTexts = document.querySelectorAll('.contacts__term-text');
+const buttons = document.querySelectorAll(".accordion__icon");
+const termTexts = document.querySelectorAll(".contacts__term-text");
 
 buttons.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     const termText = termTexts[index]; // Знаходимо відповідний текстовий блок за індексом
-    if (termText.style.display === 'block') {
-      termText.style.display = 'none';
+    if (termText.style.display === "block") {
+      termText.style.display = "none";
     } else {
-      termText.style.display = 'block';
+      termText.style.display = "block";
     }
   });
 });
-
